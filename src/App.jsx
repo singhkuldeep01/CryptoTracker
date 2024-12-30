23c89b00188775dd6c8750ba13d607cd2742d1b4
Kuldeeprime01 <7351320178k@gmail.com>
2024-12-30 22:26:15 +0530

    Navbar and Banner Added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Banner from './Components/Banner/Banner'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </>
+  )
+}
+
+export default App
