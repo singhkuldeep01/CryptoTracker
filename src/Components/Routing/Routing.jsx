6b1f6c3ea87e5f7e8dd2855c34b2a84d6d6979c1
Kuldeeprime01 <7351320178k@gmail.com>
2025-01-02 22:11:51 +0530

    Suspense and Lazy loding added

--- src/Components/Routing/Routing.jsx
+++ src/Components/Routing/Routing.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../../Pages/Home'
-import CoinDetailsPage from '../../Pages/CoinDetailsPage'
-import Layout from '../../Pages/Layout'
-import About from '../../Pages/About'
-import EasterEgg from '../../Pages/EasterEgg'
+import React, { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import CoinTableSkeleton from '../Skeleton/CoinTableSkeleton';
+
+const Home = lazy(() => import('../../Pages/Home'));
+const CoinDetailsPage = lazy(() => import('../../Pages/CoinDetailsPage'));
+const Layout = lazy(() => import('../../Pages/Layout'));
+const About = lazy(() => import('../../Pages/About'));
+const EasterEgg = lazy(() => import('../../Pages/EasterEgg'));
+
+
 
 function Routing() {
   return (
-    <Routes>
-        <Route path="/" element={< Layout/>}>
-            <Route index element={<Home />} />
-            <Route path="/coin/:coinId" element={<CoinDetailsPage />} />
-            <Route path='/about' element={<About/>}/>
-            <Route path='/easteregg' element={<EasterEgg/>}></Route>
+    <Suspense fallback={<CoinTableSkeleton />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/coin/:coinId" element={<CoinDetailsPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/easteregg" element={<EasterEgg />} />
         </Route>
-    </Routes>
-  )
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default Routing
+export default Routing;
